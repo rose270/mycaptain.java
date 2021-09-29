bec6d25ccee664c00e300ad0ec6ee61cd48ee373
rose270 <86969787+rose270@users.noreply.github.com>
2021-09-29 20:52:37 +0530

    Update fibo.java

--- fibo.java
+++ fibo.java
@@ -2,7 +2,7 @@ class Fibonacci{
 public static void main(String args[])  
 {    
  int n1=0,n2=1,n3,i,count=10;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
+ System.out.print(n1+" "+n2);//   
     
  for(i=2;i<count;++i)// 
  {    
